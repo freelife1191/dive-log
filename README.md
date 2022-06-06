4612b7216440ca6e7144509b1639eaede58ade9b
kane <kane@shoplive.cloud>
2022-06-06 19:13:46 +0900

    최초 커밋

---
+++ README.md
@@ -0,0 +1,5 @@
+# SpringRunner Class 201: Spring Boot Essential
+
+스프링 부트로 구현하는 실전 멀티 모듈 프로젝트 1
+
+Div Log
---
+++ src/test/java/com/freelife/divelog/DivelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freelife.divelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivelogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
