74641df0169cc3cbdbc248d6d6c07eb97a9174d2
kane <kane@shoplive.cloud>
2022-06-06 21:02:56 +0900

    SpringApplication 살펴보

--- README.md
+++ README.md
@@ -2,4 +2,32 @@
 
 스프링 부트로 구현하는 실전 멀티 모듈 프로젝트 1
 
-Div Log
+## Div Log
+
+### SpringApplication 살펴보기
+
+Console에서 Argument 전달 방법  
+
+```bash
+./gradlew bootRun --args="--app.name=dive-log-cmd"
+```
+
+Console에서 build 하는 방법  
+
+```bash
+./gradlew clean build
+```
+
+빌드시 두개의 jar가 생성되는데 plain jar파일은 일반 적인 배포형태의 jar 파일임
+
+실행가능한 jar 생성  
+
+```bash
+unzip dive-log-0.0.1-SNAPSHOT.jar -d executable-jar
+```
+
+jar파일 실행  
+
+```bash
+java -jar dive-log-0.0.1-SNAPSHOT.jar --app.name=dive-log-jar
+```
--- src/test/java/com/freelife/divelog/DivelogApplicationTests.java
+++ src/test/java/com/freelife/divelog/DivelogApplicationTests.java
@@ -1,13 +1,19 @@
 package com.freelife.divelog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(args = {"--app.name=dive-log-test"})
 class DivelogApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads(@Autowired ApplicationArguments appArguments) {
+        assertThat(appArguments.getOptionNames()).containsOnly("app.name");
+        assertThat(appArguments.getOptionValues("app.name")).containsOnly("dive-log-test");
     }
 
 }
